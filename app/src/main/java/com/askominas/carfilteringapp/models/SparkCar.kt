e7204f9a09b9c0afe649dd1ea1051f1b31847a42
Ainis <ainis@seesense.cc>
2020-10-18 11:42:01 +0300

    Added project models

---
+++ app/src/main/java/com/askominas/carfilteringapp/models/SparkCar.kt
@@ -0,0 +1,12 @@
+
+data class SparkCar (
+
+	@SerializedName("id") val id : Int,
+	@SerializedName("plateNumber") val plateNumber : String,
+	@SerializedName("location") val location : Location,
+	@SerializedName("model") val model : Model,
+	@SerializedName("batteryPercentage") val batteryPercentage : Int,
+	@SerializedName("batteryEstimatedDistance") val batteryEstimatedDistance : Int,
+	@SerializedName("isCharging") val isCharging : Boolean,
+	@SerializedName("servicePlusEGoPoints") val servicePlusEGoPoints : Int
+)
